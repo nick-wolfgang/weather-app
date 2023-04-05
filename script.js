78e0130e3922926ce8dcbc3ffbf3ea53cd6f8d47
nick-wolfgang <nickbricewolfgang@gmail.com>
2023-04-05 04:53:13 -0800

    Weather icons, temp conversion, loading display

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
  * Weather App
  * DONE: Complete getWeatherData() to return json response Promise
  * DONE: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
+ * DONEj: Complete showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
+function fahrenheitToCelsius(fahrenheit) {
+    var celsius = (fahrenheit - 32) * 5 / 9;
+  return celsius.toFixed(1);
+}
 
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
@@ -19,30 +23,46 @@ getWeatherData = (city) => {
 }
 
 searchCity = () => {
-    const city = document.getElementById('city-input').value;
-    getWeatherData(city)
-    .then((response) => {
+  const city = document.getElementById('city-input').value;
+  let searchButton = document.getElementById('search-button')
+  let spinner = document.getElementById("loading-spinner");
+  spinner.style.display = "block";
+
+    searchButton.addEventListener('click', function() {
+      getWeatherData(city)
+      .then((response) => {
         console.log(response)
         showWeatherData(response)
-    }).catch((error) => {
-        console.log('There was an error: ', error)
+        
+        // console.log(weatherIcon)
+        spinner.style.display = "none";
+      }).catch((error) => {
+        console.log('There was an error: ', error) 
+        showSearchError() 
+        spinner.style.display = "none";
+      })
     })
+    
 }
 
 
-/**
- * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
- */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
+  console.log("Hello")
   document.getElementById('city-name').innerText = weatherData.name
   document.getElementById('weather-type').innerText = weatherData.weather[0].main
-  document.getElementById('temp').innerText = weatherData.main.temp
-  document.getElementById('min-temp').innerText = weatherData.main.temp_min
-  document.getElementById('max-temp').innerText = weatherData.main.temp_max
-
-
-  
+  document.getElementById('temp').innerText = fahrenheitToCelsius(weatherData.main.temp)
+  document.getElementById('min-temp').innerText = fahrenheitToCelsius(weatherData.main.temp_min)
+  document.getElementById('max-temp').innerText = fahrenheitToCelsius(weatherData.main.temp_max)
+  document.getElementById('weather-icon').style.display = 'block'
+  const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+  const weatherIcon = document.getElementById("weather-icon")
+  weatherIcon.setAttribute("src", weatherIconUrl)
 }
 
+showSearchError = () => {
+  document.getElementById('error-title').style.display = 'block';
+  document.getElementById("error-message").style.display = "block";
+  document.getElementById('weather-type').style.display = "none";
+  document.getElementById('city-name').style.display= "none";
+  document.getElementById('weather-details').style.display= "none";
+}
